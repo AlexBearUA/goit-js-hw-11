1c7b228b68131d16d08fff79460865c6096dc0bd
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-03-09 09:02:19 +0100

    push it

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import ImagesApiService from './js/images-service';
+import LoadMoreBtn from './js/components/load-more-btn';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   imagesContainer: document.querySelector('.gallery'),
 };
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
 const imagesApiService = new ImagesApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 
 function onSearch(e) {
   e.preventDefault();
-  imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value.trim();
 
   if (imagesApiService.searchQuery === '') {
     return alert('Введи что-то нормальное');
   }
 
+  imagesApiService.resetPage();
+  clearImagesContainer();
   fetchImages();
 }
 
 function fetchImages() {
-  imagesApiService.fetchImages().then(images => {
+  loadMoreBtn.disable();
+  imagesApiService.fetchImages(loadMoreBtn).then(images => {
     appendImagesMarkup(images);
+    loadMoreBtn.enable();
   });
 }
 
@@ -64,4 +78,8 @@ function createImagesMarkup(images) {
       }
     )
     .join('');
+}
+
+function clearImagesContainer() {
+  refs.imagesContainer.innerHTML = '';
 }
--- src/js/images-service.js
+++ src/js/images-service.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const API_KEY = '34213016-753010ce7a0400954b4163a43';
 const BASE_URL = 'https://pixabay.com/api';
 
@@ -7,17 +9,20 @@ export default class ImagesApiService {
     this.page = 1;
   }
 
-  fetchImages() {
-    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+  fetchImages(loadMoreBtn) {
+    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${this.page}`;
 
     return fetch(url)
       .then(response => response.json())
       .then(data => {
         const images = data.hits;
         if (images.length === 0) {
-          return alert('Ниче не нашли');
+          return Notify.info(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
         }
         this.incrementPage();
+        loadMoreBtn.show();
         return images;
       });
   }
