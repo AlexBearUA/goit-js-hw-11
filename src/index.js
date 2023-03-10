d2169e058cec83011f904c360e5a5744c2f0e786
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-03-10 19:35:32 +0100

    ++

--- src/index.js
+++ src/index.js
@@ -56,6 +56,9 @@ function fetchImages() {
 }
 
 function appendImagesMarkup(images) {
+  if (!images) {
+    return;
+  }
   refs.imagesContainer.insertAdjacentHTML(
     'beforeend',
     createImagesMarkup(images)
--- src/js/images-service.js
+++ src/js/images-service.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+const axios = require('axios').default;
 const API_KEY = '34213016-753010ce7a0400954b4163a43';
 const BASE_URL = 'https://pixabay.com/api';
 
@@ -10,52 +10,50 @@ export default class ImagesApiService {
     this.perPage = 40;
   }
 
-  fetchImages(loadMoreBtn) {
+  async fetchImages(loadMoreBtn) {
     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
 
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const images = data.hits;
-        const totalHits = data.totalHits;
+    const data = await axios.get(url);
+    const images = data.data.hits;
+    const totalHits = data.data.totalHits;
 
-        if (images.length === 0) {
-          Notify.info(
-            'Sorry, there are no images matching your search query. Please try again.',
+    if (images.length === 0) {
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.',
+        {
+          position: 'left-top',
+        }
+      );
+      return;
+    }
+
+    if (this.page === 1) {
+      data.total <= 500
+        ? Notify.info(`Hooray! We found ${data.total} images!`, {
+            position: 'left-top',
+          })
+        : Notify.info(
+            `Hooray! We found ${data.total} images, but we will show you only ${totalHits}. Ha-ha!`,
             {
               position: 'left-top',
             }
           );
-          return;
+    }
+    console.log(this.perPage, this.page, totalHits);
+    if (this.perPage * this.page >= totalHits) {
+      loadMoreBtn.hide();
+      Notify.info(
+        "We're sorry, but you've reached the end of search results.",
+        {
+          position: 'left-top',
         }
+      );
+      return images;
+    }
 
-        if (this.page === 1) {
-          data.total <= 500
-            ? Notify.info(`Hooray! We found ${data.total} images!`, {
-                position: 'left-top',
-              })
-            : Notify.info(
-                `Hooray! We found ${data.total} images, but we will show you only ${totalHits}. Ha-ha!`,
-                {
-                  position: 'left-top',
-                }
-              );
-        }
-
-        if (this.perPage * this.page >= totalHits) {
-          loadMoreBtn.hide();
-          Notify.info(
-            "We're sorry, but you've reached the end of search results.",
-            {
-              position: 'left-top',
-            }
-          );
-          return images;
-        }
-        this.incrementPage();
-        loadMoreBtn.show();
-        return images;
-      });
+    this.incrementPage();
+    loadMoreBtn.show();
+    return images;
   }
 
   incrementPage() {
